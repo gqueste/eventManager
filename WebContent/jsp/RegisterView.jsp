e979d96056bf5038d221f9bd06eb984553168828
gqueste <gabriel.queste@gmail.com>
2014-10-21 14:45:21 +0200

    Travail sur le formulaire de connexion : jsp ajouté dans le header

---
+++ WebContent/jsp/RegisterView.jsp
@@ -0,0 +1,3 @@
+<%@ include file="../WEB-INF/jspf/header.jspf"%>
+<h1>Page d'inscription</h1>
+<%@ include file="../WEB-INF/jspf/footer.jspf"%>
--- src/main/java/com/eventManager/servlets/LogIn.java
+++ src/main/java/com/eventManager/servlets/LogIn.java
@@ -7,36 +7,91 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class LogIn
  */
 public class LogIn extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LogIn() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LogIn() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// gere la connexion et l enregistrement d un nouvel entrant
-		RequestDispatcher rd;
-		rd = request.getRequestDispatcher("jsp/LogInView.jsp");
-		rd.forward(request, response);
+		process(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		process(request, response);
 	}
 
+	private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// gere la connexion et l enregistrement d un nouvel entrant
+		RequestDispatcher rd;
+		HttpSession session = request.getSession(false);
+		if (isSessionValid(session)) {
+			response.sendRedirect("");
+		}
+		else {
+			//pas encore connecté
+			rd = request.getRequestDispatcher("jsp/LogInView.jsp");
+
+			String paramMail = request.getParameter("mail");
+			System.out.println(paramMail);
+			String paramPassword = request.getParameter("password");
+			String paramTentative = request.getParameter("tentative");
+			System.out.println(paramTentative);
+			
+			String noError = "";
+			String erreurs = "";
+			
+			if (paramTentative != null) {
+				//tentative de connexion
+				if(paramMail.equals("") || paramPassword.equals("")) {
+					//champ manquant
+					if(paramMail.equals("")) {
+						erreurs += "Entrez votre adresse mail pour vous connecter. ";
+					}
+					else {
+						//TODO mail existe ?
+					}
+					
+					if(paramPassword.equals("")) {
+						erreurs += "Entrez votre mot de passe pour vous connecter.";
+					}
+				}
+				else{
+					//vérification si données entrées sont bonnes
+				}
+			}
+			
+			request.setAttribute("noError", noError);
+			request.setAttribute("erreurs", erreurs);
+			
+			rd.forward(request, response);
+		}
+	}
+
+	private boolean isSessionValid(HttpSession session) {
+		boolean ret = false;
+		if(session != null) {
+			String idUtilisateur = (String) session.getAttribute("id_user");
+			if(idUtilisateur != null) {
+				ret = true;
+			}
+		}
+		return ret;
+	}
 }
