304710d3342845c977eb476e13a6addd4fd8a5c8
gqueste <gabriel.queste@gmail.com>
2014-10-22 16:44:56 +0200

    LogOut

---
+++ src/main/java/com/eventManager/servlets/LogOut.java
@@ -0,0 +1,50 @@
+package com.eventManager.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.eventManager.utils.ConnexionUtils;
+
+/**
+ * Servlet implementation class LogOut
+ */
+public class LogOut extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LogOut() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request, response);
+	}
+
+	private void process (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession(false);
+		if(ConnexionUtils.isSessionValid(request)) {
+			session.invalidate();
+			System.out.println("Session détruite");
+		}
+		response.sendRedirect("");		
+	}
+
+}
