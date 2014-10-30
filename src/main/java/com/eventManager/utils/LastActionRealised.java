37d8ee0d25a347c99beea28d450b21ccf2805ef0
gqueste <gabriel.queste@gmail.com>
2014-10-30 11:26:09 +0100

    LastActionRealised passé en singleton

--- src/main/java/com/eventManager/utils/LastActionRealised.java
+++ src/main/java/com/eventManager/utils/LastActionRealised.java
@@ -1,8 +1,22 @@
 package com.eventManager.utils;
 
-public class LastActionRealised {
+public final class LastActionRealised {
 	private String lastAction ="";
-
+	private static volatile LastActionRealised instance = null;
+	
+	private LastActionRealised() {}
+	
+	public final static LastActionRealised getInstance(){
+		if(LastActionRealised.instance == null) {
+			synchronized (LastActionRealised.class) {
+				if(LastActionRealised.instance == null) {
+					LastActionRealised.instance = new LastActionRealised();
+				}
+			}
+		}
+		return LastActionRealised.instance;
+	}
+		
 	public String getLastAction() {
 		return lastAction;
 	}
